e612f2ff7a29b9b5e071f39d93308c2a79ee0663
Asia K <asialakaygrady@gmail.com>
2023-03-27 01:19:20 -0400

    moved css file to it's own folder and added loading component

--- pages/api/generate.js
+++ pages/api/generate.js
@@ -1,12 +1,19 @@
+// Import the Configuration and OpenAIApi classes from the "openai" library
 import { Configuration, OpenAIApi } from "openai";
 
+// Create a new Configuration object with the OPENAI_API_KEY environment variable as the API key
 const configuration = new Configuration({
   apiKey: process.env.OPENAI_API_KEY,
 });
+
+// Create a new OpenAIApi object with the Configuration object as the configuration
 const openai = new OpenAIApi(configuration);
 
+// Export an asynchronous function as the default export of the module that handles incoming HTTP requests to the API endpoint
 export default async function (req, res) {
+  // Check if the OPENAI_API_KEY environment variable is set
   if (!configuration.apiKey) {
+    // If it's not set, return a 500 error response with an error message
     res.status(500).json({
       error: {
         message: "OpenAI API key not configured, please follow instructions in README.md",
@@ -15,8 +22,10 @@ export default async function (req, res) {
     return;
   }
 
+  // Check the "animal" field of the HTTP request body to ensure it's not empty
   const animal = req.body.animal || '';
   if (animal.trim().length === 0) {
+    // If it's empty, return a 400 error response with an error message
     res.status(400).json({
       error: {
         message: "Please enter a valid animal",
@@ -26,14 +35,16 @@ export default async function (req, res) {
   }
 
   try {
+    // Create a new OpenAI completion request with the "model", "prompt", and "temperature" parameters set
     const completion = await openai.createCompletion({
       model: "text-davinci-003",
       prompt: generatePrompt(animal),
       temperature: 0.6,
     });
+    // Extract the first generated name from the "choices" array in the completion response and return it as the result of the API request in a JSON object
     res.status(200).json({ result: completion.data.choices[0].text });
   } catch(error) {
-    // Consider adjusting the error handling logic for your use case
+    // If an error occurs, handle it and return a JSON object containing an error message
     if (error.response) {
       console.error(error.response.status, error.response.data);
       res.status(error.response.status).json(error.response.data);
@@ -48,6 +59,7 @@ export default async function (req, res) {
   }
 }
 
+// Define a function that takes an animal parameter and returns a string containing a prompt for generating superhero animal names
 function generatePrompt(animal) {
   const capitalizedAnimal =
     animal[0].toUpperCase() + animal.slice(1).toLowerCase();
--- pages/index.js
+++ pages/index.js
@@ -1,14 +1,23 @@
+// Import the Head component from the "next/head" module and the useState hook from the "react" module
 import Head from "next/head";
 import { useState } from "react";
-import styles from "./index.module.css";
+// Import the CSS styles from the "index.module.css" module
+import styles from "../styles/index.module.css";
 
+// Define a function component called Home that serves as the main page of the application
 export default function Home() {
+  // Define two state variables using the useState hook: animalInput and result
   const [animalInput, setAnimalInput] = useState("");
   const [result, setResult] = useState();
-
+  // state variable isLoading using the useState hook
+  const [isLoading, setIsLoading] = useState(false);
+  
+// Define an asynchronous function called onSubmit that handles the form submission event
   async function onSubmit(event) {
     event.preventDefault();
+    setIsLoading(true);
     try {
+      // Send a POST request to the "/api/generate" endpoint with the animalInput value in the request body as a JSON object
       const response = await fetch("/api/generate", {
         method: "POST",
         headers: {
@@ -17,20 +26,32 @@ export default function Home() {
         body: JSON.stringify({ animal: animalInput }),
       });
 
+      // Parse the response body as a JSON object
       const data = await response.json();
+
+      // Check the response status code; if it's not 200, throw an error with the message from the server response
       if (response.status !== 200) {
         throw data.error || new Error(`Request failed with status ${response.status}`);
       }
 
+      // Update the result state variable with the generated name, and reset the animalInput state variable to an empty string
       setResult(data.result);
       setAnimalInput("");
     } catch(error) {
-      // Consider implementing your own error handling logic here
+      // If an error occurs during the process, log the error to the console and display an alert with the error message
       console.error(error);
       alert(error.message);
+    } finally {
+      // set the isLoading state to true when the API request starts and to false when it completes or if an error occurs:
+      setIsLoading(false);
     }
   }
 
+  // loading indicator component 
+  function LoadingIndicator(){
+    return <div>Loading...</div>
+  }
+  // Return the HTML structure of the main page, including a Head component with the page title and icon, a main section with a form and a result section, and CSS classes defined in the "index.module.css" module
   return (
     <div>
       <Head>
@@ -51,7 +72,10 @@ export default function Home() {
           />
           <input type="submit" value="Generate names" />
         </form>
-        <div className={styles.result}>{result}</div>
+        <div className={styles.result}>
+          {/* conditionally render the LoadingIndicator component based on the isLoading state */}
+          {isLoading ? <LoadingIndicator /> : result }
+          </div>
       </main>
     </div>
   );
